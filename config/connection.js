9f1e271ea153b5da7a5d2279a328aeb88d5350b5
Tyler Brooks <thetylerbrooks@gmail.com>
2023-10-10 15:17:12 -0600

    .env added, pg and env dependencies added, connection.js setup

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,52 @@
-const mongoose = require("mongoose");
+// a connection pool allows you to make multiple connection requests to your server (neccessary?)
+// const { Pool } = require("pg");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/poker", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const connectDb = async () => {
+//   try {
+//     const pool = new Pool({
+//       user: process.env.PGUSER,
+//       host: process.env.PGHOST,
+//       database: process.env.PGDATABASE,
+//       password: process.env.PGPASSWORD,
+//       port: process.env.PGPORT,
+//     });
 
-module.exports = mongoose.connection;
+//     await pool.connect();
+//     const res = await pool.query("SELECT * FROM clients");
+//     console.log(res);
+//     await pool.end();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// connectDb();
+
+//single client connection
+
+const { Client } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectDb = async () => {
+  try {
+    const client = new Client({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+    });
+
+    await client.connect();
+    const res = await client.query("SELECT * FROM some_table");
+    console.log(res);
+    await client.end();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectDb();
