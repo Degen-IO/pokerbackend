23e3455237e925c01e0feb31fa3f38807ec72cb0
Pat Brown <breepatbree@gmail.com>
2023-12-07 20:23:47 -0700

    Explanation of functionality

--- redis/subscribers.js
+++ redis/subscribers.js
@@ -1,5 +1,13 @@
 const { redisSubscriber } = require("../config/redis");
+/*
+This file is responsible for listening to various Redis channels related to the game and responding to messages published on those channels.
 
+When a message is received on any of these channels, the subscriber can perform appropriate actions based on the message content. 
+This could involve updating the backend game state, notifying other players, or triggering other game logic.
+
+
+Allows the server to react in real-time to actions and changes in the game, maintaining an up-to-date and synchronized state across all participants.
+*/
 redisSubscriber.subscribe("testChannel", (err, count) => {
   if (err) {
     console.error("Failed to subscribe: %s", err.message);
