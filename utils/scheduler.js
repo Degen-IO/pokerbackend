02894d338020176b6ad6fdb04c6e7db7009e044b
Tyler Brooks <thetylerbrooks@gmail.com>
2024-04-16 21:41:20 -0600

    game status changes to -starting- 1min before startDateTime and -ongoing- on startdateTime

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -3,40 +3,42 @@ const { Op } = require("sequelize");
 const { Mutation } = require("../schemas/resolvers");
 const { CashGame, TournamentGame } = require("../models");
 
-const windowSizeMilliseconds = 3000; // 3 seconds
-
 //***** is cron syntax */
 cron.schedule("* * * * *", async () => {
   try {
     const currentDateTimeUTC = new Date();
-    const startDateTimeCondition = {
-      [Op.between]: [
-        new Date(currentDateTimeUTC.getTime() - windowSizeMilliseconds),
-        new Date(currentDateTimeUTC.getTime() + windowSizeMilliseconds),
-      ],
-    };
+    const oneMinuteBeforeStart = new Date(currentDateTimeUTC.getTime() + 60000);
+    console.log("------------Current UTC Time:", currentDateTimeUTC);
+    console.log("------------One Minute Before Start:", oneMinuteBeforeStart);
 
+    // Find games waiting to start
     const waitingCashGames = await CashGame.findAll({
       where: {
         status: "waiting",
-        startDateTime: startDateTimeCondition,
+        startDateTime: {
+          [Op.between]: [
+            new Date(oneMinuteBeforeStart.getTime() - 60000), // One minute behind
+            oneMinuteBeforeStart, // One minute ahead
+          ],
+        },
       },
     });
+    console.log("--------------------waitingCashGames", waitingCashGames);
 
     const waitingTournamentGames = await TournamentGame.findAll({
       where: {
         status: "waiting",
-        startDateTime: startDateTimeCondition,
+        startDateTime: oneMinuteBeforeStart,
       },
     });
 
-    //we pass an empty string as the parent argument
+    // Update status to "starting" for the found games
     for (const cashGame of waitingCashGames) {
       try {
         await Mutation.updateGameStatus("", {
-          gameId: cashGame.gameId,
+          gameId: cashGame.cashId,
           gameType: "cash",
-          status: "ongoing",
+          status: "starting",
         });
       } catch (error) {
         console.error("Error updating game status:", error);
@@ -46,14 +48,41 @@ cron.schedule("* * * * *", async () => {
     for (const tournamentGame of waitingTournamentGames) {
       try {
         await Mutation.updateGameStatus("", {
-          gameId: tournamentGame.gameId,
+          gameId: tournamentGame.tournamentId,
           gameType: "tournament",
-          status: "ongoing",
+          status: "starting",
         });
       } catch (error) {
         console.error("Error updating game status:", error);
       }
     }
+
+    // Schedule another task to update status from "starting" to "ongoing" after a minute
+    setTimeout(async () => {
+      for (const cashGame of waitingCashGames) {
+        try {
+          await Mutation.updateGameStatus("", {
+            gameId: cashGame.cashId,
+            gameType: "cash",
+            status: "ongoing",
+          });
+        } catch (error) {
+          console.error("Error updating game status:", error);
+        }
+      }
+
+      for (const tournamentGame of waitingTournamentGames) {
+        try {
+          await Mutation.updateGameStatus("", {
+            gameId: tournamentGame.tournamentId,
+            gameType: "tournament",
+            status: "ongoing",
+          });
+        } catch (error) {
+          console.error("Error updating game status:", error);
+        }
+      }
+    }, 60000);
   } catch (error) {
     console.error("Failed to update game status:", error);
   }
