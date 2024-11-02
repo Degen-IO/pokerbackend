797a46d3198b45bf7303cb9edda0f7729e22891f
Pat Brown <breepatbree@gmail.com>
2024-11-01 22:59:16 -0600

    Subscriber commenting

--- redis/subscribers.js
+++ redis/subscribers.js
@@ -1,11 +1,16 @@
 // redis/subscribers.js
+/*
+This file is responsible for listening to various Redis channels related to the game and responding to messages published on those channels.
+
+When a message is received on any of these channels, the subscriber can perform appropriate actions based on the message content. 
+This could involve updating the backend game state, notifying other players, or triggering other game logic. */
 const { redisSubscriber } = require("../config/redis");
 
 if (!redisSubscriber) {
   console.error("redisSubscriber is undefined. Check Redis configuration.");
   process.exit(1); // Exit the process if redisSubscriber is unavailable
 } else {
-  // Subscribe to channels
+  // Subscribe to global game channel
   redisSubscriber.subscribe("game:*", (err, count) => {
     if (err) {
       console.error("Failed to subscribe to game channel:", err.message);
@@ -14,6 +19,7 @@ if (!redisSubscriber) {
     }
   });
 
+  //Allows the server to react in real-time to actions and changes in the game, maintaining an up-to-date and synchronized state across all participants.
   redisSubscriber.subscribe("testChannel", (err, count) => {
     if (err) {
       console.error("Failed to subscribe to testChannel:", err.message);
@@ -26,6 +32,7 @@ if (!redisSubscriber) {
 
   redisSubscriber.on("message", (channel, message) => {
     console.log(`Received message from ${channel}: ${message}`);
-    // Handle different types of messages here
+    // Handle different types of messages (e.g., CARD_DISTRIBUTION)
+    // Update the backend game state, notify other players, or trigger game logic
   });
 }
